a5cdb5ebec64b1a8c1d4457edb3547606c5c88fb
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-11-27 12:26:39 -0300

    update

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@ const dados = '{"id": 1, "nome": "elli", "idade": 17,  "girlfriend": true}';
 const obj = JSON.parse(dados)
 
 function teste(){
-    while(obj.girlfriend == true){
-        const h1 = document.getElementById("h1").innerHTML = obj.girlfriend;
+    if(obj.girlfriend == true){
+        const h1 = document.getElementById("h1").innerHTML = obj.id;
     }
 }
 teste();
