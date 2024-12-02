b94bb798ff14f2564081496c2c19539e44c35498
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-12-02 18:07:33 -0300

    update

--- app/app.js
+++ app/app.js
@@ -2,13 +2,19 @@
 const dados = '{"id": 1, "nome": "elli", "idade": 17,  "girlfriend": true}';
 const obj = JSON.parse(dados)
 
-function teste(){
-    if(obj.girlfriend == true){
-        const h1 = document.getElementById("h1").innerHTML = obj.id;
-    }
+// console.log(obj.id)
+
+// if(obj.girlfriend === true){
+//     const h1 = document.getElementById("h1").innerHTML = obj.id;
+// }
+
+
+
+
+
+const id = prompt("digite numero")
+const numAlea = Math.floor(Math.random() * 10) + 1;
+
+if(obj.id == numAlea){
+    console.log("o numero não é igual")
 }
-teste();
-
-
-// const jsonString = JSON.stringify(obj);
-// console.log(jsonString)
